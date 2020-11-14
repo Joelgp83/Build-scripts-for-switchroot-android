a0188ad411eb98c81f254448784c0cc271d8cae2
Joelgp83 <Joelgp83@users.noreply.github.com>
2020-11-14 01:53:42 -0800

    Add new source patches from Q-Tips Guide

--- qupd8.sh
+++ qupd8.sh
@@ -79,7 +79,12 @@ patch -p1 < ../../../.repo/android_device_nvidia_foster.patch
 rm ../../../.repo/android_device_nvidia_foster.patch
 cd ../../../bionic
 patch -p1 < ../.repo/local_manifests/patches/bionic_intrinsics.patch
-cd ../
+cd ../frameworks/native
+patch -p1 < ../../.repo/local_manifests/patches/frameworks_native_hwc.patch
+cd ../../hardware/libhardware
+patch -p1 < ../../.repo/local_manifests/patches/hardware_libhardware_layers.patch
+cd ../../
+
 
 echo "Source downloaded and patched.  Moving on to environment setup."
 #Setup ccache
